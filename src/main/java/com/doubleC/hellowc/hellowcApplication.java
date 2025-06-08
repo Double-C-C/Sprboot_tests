9f7787f8e6fb8c4f04a84641d94eff6da0ea6725
Double-C-C <xfy13066344092@outlook.com>
2025-06-08 12:41:55 +0800

    Initial commit

---
+++ src/main/java/com/doubleC/hellowc/hellowcApplication.java
@@ -0,0 +1,20 @@
+package com.doubleC.hellowc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController // 直接写在这里，不建 controller 文件夹
+public class hellowcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(hellowcApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "你好，二极管！";
+    }
+}
